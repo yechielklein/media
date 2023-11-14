87a65a2d7288361d3ab72465b19b3e60da4558c2
Yechiel Klein <kleinyechiel@gmail.com>
2023-11-13 22:58:47 -0500

    setup media app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const rootElement = document.querySelector('#root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<App />);
